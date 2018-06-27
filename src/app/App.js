c8afc3434d8461156cad6755396638064a9f45bd
Xuehui Chen <jeffreychen2016@gmail.com>
2018-06-27 17:37:29 -0500

    added GET function

--- src/app/App.js
+++ src/app/App.js
@@ -4,16 +4,28 @@ import connection from '../firebaseRequests/connection';
 import Listings from '../components/Listings/Listing';
 import Building from '../components/Building/Building';
 import ListingForm from '../components/ListingForm/ListingForm';
+import listingRequests from '../firebaseRequests/listings';
 
 class App extends Component {
+  state = {
+    listings: [],
+  }
+
   componentDidMount () {
     connection();
+    listingRequests.getRequest()
+      .then((listings) => {
+        this.setState({listings});
+      })
+      .catch((err) => {
+        console.error('Error with listing GET',err);
+      });
   };
   render () {
     return (
       <div className="App">
         <div className="col-sm-6">
-          <Listings />
+          <Listings listings={this.state.listings}/>
         </div>
         <div className="col-sm-6">
           <Building />
--- src/components/Listings/Listing.js
+++ src/components/Listings/Listing.js
@@ -4,9 +4,20 @@ import './Listings.css';
 
 class Listings extends React.Component {
   render () {
+    // const {listings} = this.props.listings;
+    const {listings} = this.props;
+    const listingsItemComponents = listings.map((listingItem) => {
+      return (
+        <li key={listingItem.id}>{listingItem.price}</li>
+      );
+    });
+
     return (
       <div className="Listings">
         <h2>Listings</h2>
+        <ul>
+          {listingsItemComponents}
+        </ul>
       </div>
     );
   }
