18b567bbb94318edbce7be3d74fe9da1ea6a2494
Xuehui Chen <jeffreychen2016@gmail.com>
2018-06-30 11:20:24 -0500

    added ListingItem component

--- src/components/Listings/Listing.js
+++ src/components/Listings/Listing.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {listingShape} from '../../propz/listingProp';
-
+import ListingItem from '../../components/ListingItem/ListingItem';
 import './Listings.css';
 
 class Listings extends React.Component {
 
+  // check in-coming props to see if the data type is what we expected
   static propTypes = {
     listings: PropTypes.arrayOf(listingShape),
   };
@@ -15,7 +16,9 @@ class Listings extends React.Component {
     const {listings} = this.props;
     const listingsItemComponents = listings.map((listingItem) => {
       return (
-        <li key={listingItem.id}>{listingItem.price}</li>
+        // move this to seperate component
+        // <li key={listingItem.id}>{listingItem.price}</li>
+        <ListingItem listing={listingItem} key={listingItem.id}/>
       );
     });
 
